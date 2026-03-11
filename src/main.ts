4af47687c3847cbed2ebc0aa811b27900341f73f
Wanderson PJ <wandersonpj_bkn@hotmail.com>
2026-03-11 18:35:42 -0300

    + chore: changes Amazon SVG
    + chore: moves SCSS files to a specific folder

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import router from '@/router'
 
 import App from '@/App.vue'
 
-import '@/assets/main.scss'
+import '@/assets/scss/main.scss'
 
 const app = createApp(App)
 const pinia = createPinia()
