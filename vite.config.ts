aae94b279dd6e07f508a3ca6f7f5b8f95fc3eb81
Wanderson PJ <wandersonpj_bkn@hotmail.com>
2026-03-13 03:10:08 -0300

    chore: adds PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,57 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import vue from '@vitejs/plugin-vue'
 import svgLoader from 'vite-svg-loader'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), svgLoader(), vueDevTools()],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: 'Ecos Catalog',
+        short_name: 'EcosCatalog',
+        description: 'Ecos Literários Catalog',
+        theme_color: '#5784E6',
+        background_color: '#F4D1FF',
+        display: 'fullscreen',
+        orientation: 'portrait',
+        lang: 'pt-BR',
+        icons: [
+          {
+            src: '/icons/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/icons/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: '/icons/icon-1024.png',
+            sizes: '1024x1024',
+            type: 'image/png',
+          },
+          {
+            src: '/icons/icon-maskable-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+      },
+    }),
+    svgLoader(),
+    vueDevTools(),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
